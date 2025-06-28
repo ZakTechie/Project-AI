00e737e556295664c42fdc40e9aded06854b11f7
ElZekoo <zakaria.ahmed6765@gmail.com>
2025-06-28 15:59:53 +0300

    updated

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -391,6 +391,10 @@ const Dashboard = () => {
                       {title.map((topic, j) =>
                         title.length > 0 ? (
                           <li
+                            style={{
+                              display: "flex",
+                              justifyContent: "space-between",
+                            }}
                             key={j}
                             onClick={(e) => {
                               e.stopPropagation();
@@ -401,6 +405,25 @@ const Dashboard = () => {
                             }
                           >
                             {topic.LectureName}
+                            <svg
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                handleDeleteCourse(course.courseId);
+                              }}
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="16"
+                              height="16"
+                              fill="white"
+                              viewBox="0 0 16 16"
+                              style={{ cursor: "pointer", marginLeft: "8px" }}
+                              title="حذف"
+                            >
+                              <path d="M5.5 5.5a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0v-6a.5.5 0 0 1 .5-.5zm2.5.5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0v-6zm1.5-.5a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0v-6a.5.5 0 0 1 .5-.5z" />
+                              <path
+                                fillRule="evenodd"
+                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1 0-2h3.086a1 1 0 0 1 .707.293l.707.707h2.586l.707-.707A1 1 0 0 1 9.414 1H12.5a1 1 0 0 1 1 1v1h1a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3a.5.5 0 0 0 0 1h11a.5.5 0 0 0 0-1h-11z"
+                              />
+                            </svg>
                           </li>
                         ) : (
                           <li
